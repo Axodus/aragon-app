fdf57c6359f29a06c5d89452710d09847531aee4
Mauricio Z. Filho <contact@thinkincoin.com>
2026-01-04 15:59:11 -0300

    feat: substituir interface por tipo para o formulário de configuração de governança no Harmony

--- src/plugins/harmonyVotingPlugin/components/harmonyVotingSetupGovernance/harmonyVotingSetupGovernance.api.ts
+++ src/plugins/harmonyVotingPlugin/components/harmonyVotingSetupGovernance/harmonyVotingSetupGovernance.api.ts
@@ -1,7 +1,6 @@
 import type { IPluginSetupGovernanceParams } from '@/modules/createDao/types';
 
-export interface IHarmonyVotingSetupGovernanceForm {
-}
+export type IHarmonyVotingSetupGovernanceForm = Record<string, never>;
 
 export interface IHarmonyVotingSetupGovernanceProps extends IPluginSetupGovernanceParams {
     daoId?: string;
