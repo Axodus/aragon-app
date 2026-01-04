050bcd70fa1ac0b6d1b6b7e948d0c04a746ca574
Mauricio Z. Filho <contact@thinkincoin.com>
2026-01-04 15:57:00 -0300

    feat: atualizar texto de ajuda e remover campos desnecessários na configuração de governança do Harmony

--- src/plugins/harmonyVotingPlugin/components/harmonyVotingSetupGovernance/harmonyVotingSetupGovernance.api.ts
+++ src/plugins/harmonyVotingPlugin/components/harmonyVotingSetupGovernance/harmonyVotingSetupGovernance.api.ts
@@ -1,8 +1,6 @@
 import type { IPluginSetupGovernanceParams } from '@/modules/createDao/types';
 
 export interface IHarmonyVotingSetupGovernanceForm {
-    proposer: string;
-    oracle: string;
 }
 
 export interface IHarmonyVotingSetupGovernanceProps extends IPluginSetupGovernanceParams {
